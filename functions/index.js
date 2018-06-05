f042f7e0bdb47ab4ccb6a128c53d203e672c30cd
Srisarguru Sridhar <srisargurusridhar@u.boisestate.edu>
2018-06-04 22:17:35 -0400

    Added all the questions will be moved to separate file in future - 20180604

--- functions/index.js
+++ functions/index.js
@@ -155,6 +155,243 @@ const guru_side_projects_content = "1. Amazon Alexa Skill - aboutGuru using AWS 
   "app for pet owners using google maps and android java \n\n 5. Cloudy with a chance of Express JS - a simple weather app using node js, ejs and expressJS \n\n 6. Patient Registration and Monitoring System - using PHP and MySql" +
   "\n\n 7. Nine Man Morris - nine morris game using java GUI which can be played against an AI \n\n 8. Data Model of Cricket Leagues - a data model of IPL league using TOAD data modeler";
 
+//=========================================================================================================================
+//Questions
+//========================================================================================================================
+const QUESTIONS = [
+        {
+            'What is guru\'s favorite movie?': [
+                'The Prestige',
+                'The Dark Knight',
+                'The Avengers',
+                'Titanic',
+                'Dunkirk',
+                'Kabali',
+                'Logan'
+            ],
+        },
+        {
+            'Who is guru\'s favorite sportsperson?': [
+                'M.S. Dhoni',
+                'Sachin Tendulkar',
+                'Usain Bolt',
+                'Rafael Nadal',
+                'LeBron James',
+                'Christiano Ronaldo',
+                'Lionel Messi'
+            ],
+        },
+        {
+            'What does guru do for a living?': [
+                'Developer',
+                'Tester',
+                'Jobless',
+                'Doctor',
+                'Teacher',
+                'Farmer',
+                'Movie Director'
+            ],
+        },
+        {
+            'What car does guru like the most?': [
+                'Ford Mustang',
+                'Chevi Camero',
+                'Dodge Challenger',
+                'Tesla',
+                'Audi R8',
+                'BMW',
+                'Mercedes Benz'
+            ],
+        },
+        {
+            'Which of the following are guru\'s favorite colors?': [
+                'Red and Black',
+                'Black and Blue',
+                'Red and Yellow',
+                'Brown and Blue',
+                'Red and Blue',
+                'Green and Purple',
+                'Pink and Gold'
+            ],
+        },
+        {
+            'In which country was guru born?': [
+                'India',
+                'USA',
+                'Australia',
+                'Germany',
+                'Brazil',
+                'Japan',
+                'South Africa'
+            ],
+        },
+        {
+            'Which major did guru study?': [
+                'Computer Science',
+                'Electrical Engineering',
+                'Material Science',
+                'Civil Engineering',
+                'Accounting',
+                'Health Science',
+                'Mathematics'
+            ],
+        },
+        {
+            'Which of the following  musicians or bands does guru like?': [
+                'Ed Sheeran, Bruno Mars, A.R.Rahman and Frank Sinatra',
+                'Ed Sheeran, Bruno Mars, Michael Jackson and Anirudh',
+                'Eminem, Bruno Mars, A.R.Rahman and Anirudh',
+                'Ed Sheeran, Maroon five, A.R.Rahman and Anirudh',
+                'Ed Sheeran, Bruno Mars, A.R.Rahman and Anirudh',
+                'Taylor Swift, Katy Perry and Rihanna',
+                'Coldplay, Imagine dragons and Akon'
+            ],
+        },
+        {
+            'Which of the following sports that guru likes to watch?': [
+                'Cricket, American Football and Hockey',
+                'Soccer and Cricket',
+                'Cricket, Tennis and Soccer',
+                'Cricket, American Football and Basketball',
+                'Basketball, Cricket and Rugby',
+                'Cricket and Baseball',
+                'Cricket, American Football and Boxing'
+            ],
+        },
+        {
+            'Which of the following actor or actors does guru like?': [
+                'Hugh Jackman, Rajnikanth and Emma Watson',
+                'Christian Bale and Emma Stone ',
+                'Robert Downey Junior and Scarlet Johanson',
+                'Rajnikanth and Amitab Bachan',
+                'Tom Cruise',
+                'Jim Carry and Will Smith',
+                'None of the options'
+            ],
+        },
+        {
+            'Who is guru\'s favorite superhero?': [
+                'The Wolverine',
+                'Batman',
+                'Iron-man',
+                'Spider-man',
+                'Black Panther',
+                'The Flash',
+                'Thor'
+            ],
+        },
+        {
+            'What is guru\'s mother tongue?': [
+                'Tamil',
+                'Hindi',
+                'Telugu',
+                'Spanish',
+                'Chinese',
+                'English',
+                'French'
+            ],
+        },
+        {
+            'Who is guru\'s favorite and inspirational leader?': [
+                'A.P.J Abdul Kalam',
+                'Nelson Mandela',
+                'Martin Luther King',
+                'Mahatma Gandhi',
+                'Abraham Lincoln',
+                'Mother Teresa',
+                'Alexandar The Great'
+            ],
+        },
+        {
+            'Which is guru\'s favorite place?': [
+                'Boise, Idaho, USA',
+                'Paris, France',
+                'Amsterdam, Netherlands',
+                'Seattle, Washington, USA',
+                'Rio De Janeiro, Brazil',
+                'Seoul, South Korea',
+                'Tokyo, Japan'
+            ],
+        },
+        {
+            'Which is guru\'s favorite season?': [
+                'Spring',
+                'Summer',
+                'Fall',
+                'Winter',
+                'All seasons',
+                'None of them',
+                'Every season except winter'
+            ],
+        },
+        {
+            'Which is guru\'s favorite Tv Show?': [
+                'Breaking Bad',
+                'Grey\'s Anatomy',
+                'The One Hundred',
+                'Doctor House',
+                'Game of Thrones',
+                'Narcos',
+                'Suits'
+            ],
+        },
+        {
+            'Which video game does guru like the most?': [
+                'Horizon Zero Dawn',
+                'Far Cry 4',
+                'Call of Duty Black Ops three',
+                'Batman Arkham Knight',
+                'Assasin\'s Creed Syndicate',
+                'Ghost Recon Wildlands',
+                'The Witcher three'
+            ],
+        },
+        {
+            'Which is guru\'s favorite College Football Team?': [
+                'Boise State Broncos',
+                'Michigan Wolverines',
+                'Clemson Tiger',
+                'Alabama Crimsontide',
+                'Ohio State Buckeyes',
+                'Wisconsin Badgers',
+                'Georgia Bulldogs'
+            ],
+        },
+        {
+            'Which of the following are one of his favorite songs?': [
+                'Strangers in the night by Frank Sinatra',
+                'Uptown Funk by Bruno Mars',
+                'Let Her Go by Passengers',
+                'Roar by Katy Perry',
+                'Why this Kolaveri Di by Anirudh',
+                'Sugar by Maroon 5',
+                'Perfect by Ed Sheeran'
+            ],
+        },
+        {
+            'Which is guru\'s favorite NFL Team?': [
+                'Dallas Cowboys',
+                'Seattle Seahawks',
+                'Cleaveland Browns',
+                'Cincinnati Bengals',
+                'Denver Broncos',
+                'Miami Dolphins',
+                'Philadelphia Eagles'
+            ],
+        },
+        {
+            'Which is guru\'s favorite NHL Team?': [
+                'Columbus Blue Jackets',
+                'Washington Capitols',
+                'Pittsburg Penguins',
+                'L.A. Kings',
+                'New York Rangers',
+                'Nashville Predators',
+                'Boston Bruins'
+            ],
+        },
+    ];
+
 //==========================================================================================
 //FOR GURU TRIVIA
 //===========================================================================================
